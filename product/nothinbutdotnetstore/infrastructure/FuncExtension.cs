d4767c077a256f94cd5902a382eeafd78baad892
Chris Farmer <chris.farmer@gmail.com>
2010-07-29 20:09:49 -0700

    caching singleton

--- product/nothinbutdotnetstore/infrastructure/FuncExtension.cs
+++ product/nothinbutdotnetstore/infrastructure/FuncExtension.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.infrastructure
 {
     public static class FuncExtension
     {
-        public static Func<T> cache_result<T>(this Func<T> factory)
+        public static Func<T> cache_result<T>(this Func<T> factory) where T : class
         {
-            return factory;
+            T cached = null;
+            return () =>
+                       {
+                           if (cached == null)
+                               cached = factory();
+                           return cached;
+                           //return factory();
+                       };
         }
     }
 }
