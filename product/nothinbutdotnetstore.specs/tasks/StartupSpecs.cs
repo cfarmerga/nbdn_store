f2cf1d3b3f1752311902ac9a1cf4dc4b05b6e99d
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-29 23:22:55 -0400

    Starting the startup exercise

---
+++ product/nothinbutdotnetstore.specs/tasks/StartupSpecs.cs
@@ -0,0 +1,31 @@
+ 
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.infrastructure.containers;
+using nothinbutdotnetstore.tasks.startup;
+using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore.specs.tasks
+{
+    public class StartupSpecs
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        [Subject(typeof(Startup))]
+        public class when_the_application_has_completed_startup : concern
+        {
+            Because b = () =>
+                Startup.run();
+
+            It should_be_ready_to_run_the_application = () =>
+            {
+                Container.retrieve.an<FrontController>().ShouldBeAn<DefaultFrontController>();
+            };
+
+
+        }
+    }
+}
